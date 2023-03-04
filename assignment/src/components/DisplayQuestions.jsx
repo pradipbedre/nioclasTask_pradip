66f77033efa77dba6c8e74f0c330a3c6f5813351
Pradip Bedre <bedrepradip0@gmail.com>
2023-03-04 23:07:32 +0530

    basic functionality compleated

--- assignment/src/components/DisplayQuestions.jsx
+++ assignment/src/components/DisplayQuestions.jsx
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Question from "./Question";
 const DisplayQuestions = () => {
+  const [questionID, setQuestionID] = useState("AreaUnderTheCurve_901");
+
+  let Questions = [
+    "AreaUnderTheCurve_901",
+    "BinomialTheorem_901",
+    "DifferentialCalculus2_901",
+  ];
+
+  const nextQuestionHandel = () => {
+    const currentIndex = Questions.indexOf(questionID);
+    if (currentIndex === Questions.length - 1) {
+      // If current question is the last question, set question ID to first question
+      setQuestionID(Questions[0]);
+    } else {
+      // Otherwise, set question ID to the next question
+      setQuestionID(Questions[currentIndex + 1]);
+    }
+  };
+  const previousQuestionHandel = () => {
+    const currentIndex = Questions.indexOf(questionID);
+    if (currentIndex === 0) {
+      // If current question is the first question, set question ID to last question
+      setQuestionID(Questions[Questions.length - 1]);
+    } else {
+      // Otherwise, set question ID to the previous question
+      setQuestionID(Questions[currentIndex - 1]);
+    }
+  };
+
   return (
     <div className="display_questions">
       <div className="display_questions__question">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa minima
-          exercitationem nam dolorem labore nulla quis amet molestiae a illo
-          veniam consequuntur reprehenderit architecto doloribus, dolor
-          perferendis. Saepe, perferendis iste!
-        </p>
+        <Question QuestionID={questionID} />
       </div>
       <div className="display_questions__buttons">
-        <button className="display_questions__buttons__previous">
+        <button
+          className="display_questions__buttons__previous"
+          onClick={previousQuestionHandel}
+        >
           Previous
         </button>
-        <button className="display_questions__buttons__next">Next</button>
+        <button
+          className="display_questions__buttons__next"
+          onClick={nextQuestionHandel}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
--- assignment/src/components/Question.jsx
+++ assignment/src/components/Question.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
-const Question = () => {
+import React, { useEffect, useState } from "react";
+import { MathJax, MathJaxContext } from "better-react-mathjax";
+const Question = ({ QuestionID }) => {
+  const [question, setQuestion] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await fetch(
+          `https://0h8nti4f08.execute-api.ap-northeast-1.amazonaws.com/getQuestionDetails/getquestiondetails?QuestionID=${QuestionID}`
+        )
+          .then((data) => data.json())
+          .then((data) => setQuestion(data));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [QuestionID]);
+  // console.log(question);
   return (
-    <div>Question</div>
-  )
-}
+    <MathJaxContext>
+      <div className="question">
+        {question && <MathJax>{question[0].Question}</MathJax>}
+      </div>
+    </MathJaxContext>
+  );
+};
 
 export default Question;
