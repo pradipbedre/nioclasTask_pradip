97bda8671136bf7b032a36a99b7ace9f7359dbb2
Pradip Bedre <bedrepradip0@gmail.com>
2023-03-04 21:29:24 +0530

    basic app setup is ready with components.

---
+++ assignment/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DisplayQuestions from "./components/DisplayQuestions";
+
+const App = () => {
+  return (
+    <div className="App">
+      <DisplayQuestions />
+    </div>
+  );
+};
+
+export default App;
---
+++ assignment/src/components/DisplayQuestions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Question from "./Question";
+const DisplayQuestions = () => {
+  return (
+    <div>
+      {" "}
+      <Question />
+    </div>
+  );
+};
+
+export default DisplayQuestions;
